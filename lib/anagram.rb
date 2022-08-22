ed2b3f44d333b7308a843f0e8bed7cdc391f133d
Amen Atikpo <alaskale2024@gmail.com>
2022-08-22 20:58:44 +0000

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+        
+    end
+    def match(anagrams)
+        new_anagram = %w[]
+        anagrams.each do |anagram|
+            if anagram.chars().sort == @word.chars().sort
+                new_anagram << anagram
+            else
+                []
+            end
+        end
+        return new_anagram      
+    end
+end
