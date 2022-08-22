62e27ca745d8c276f44e82997df3fc0cba5afee0
Amen Atikpo <alaskale2024@gmail.com>
2022-08-22 20:02:48 +0000

    commit tree 27ccafc67d1ecc2c08cf614f4d0d6c37080509f7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+        
+    end
+    def match(anagrams)
+        result = Hash.new []
+        anagrams.each_with_object(hash.new[]) do |anagram, hash|
+            
+            hash[anagram.chars.sort] == @word.chars.sort
+                
+            
+            
+        end 
+         
+    end
+end
